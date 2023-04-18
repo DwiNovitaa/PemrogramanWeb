1c7c9fd18540840e6f1de9a45a2838954076b983
DwiNovitaa <115929181+DwiNovitaa@users.noreply.github.com>
2023-04-18 12:13:35 +0800

    SESI6
    SESI6

---
+++ SESI6/formlogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login User</title>
+</head>
+<body>
+<form action="register.php" method="POST">
+<div>
+    User Name
+    <input type="text" name="txUSER">
+</div>
+<div>
+    Password 
+    <input type="password" name="txPASS1">
+</div>
+    
+<div>
+    <button type="submit">Login</button>
+</div>
+
+</form>
+</body>
+</html>
